fca2c19f6b9eb429f0312a274a3e459bba230210
Philip Chang <sgnoohc@gmail.com>
2018-05-17 17:23:36 -0700

    Point to the combined GRL version

--- grl.cc
+++ grl.cc
@@ -3,7 +3,7 @@
 void CoreUtil::grl::setup()
 {
 
-    json_file = "coreutil/data/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON_snt.txt"; // 26p4 fb
+    json_file = "Cert_271036-306462_13TeV_2016and2017_23Sep2016ReReco_Collisions16_JSON_EOY2017ReReco_Collisions17_JSON_v1_snt.txt";
     cout << "Setting grl: " << json_file << endl;
     set_goodrun_file(json_file);
 
