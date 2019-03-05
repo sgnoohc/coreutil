2435232b6637545e8bed06b81aec20dd2bd0b490
Mia Liu <miaoyuan.liu0@gmail.com>
2019-03-04 19:47:27 -0800

    add changes for 2018

--- grl.cc
+++ grl.cc
@@ -3,7 +3,7 @@
 void CoreUtil::grl::setup()
 {
 
-    json_file = "coreutil/data/Cert_271036-306462_13TeV_2016and2017_23Sep2016ReReco_Collisions16_JSON_EOY2017ReReco_Collisions17_JSON_v1_snt.txt";
+    json_file = "coreutil/data/Cert_run2.txt";
     cout << "Setting grl: " << json_file << endl;
     set_goodrun_file(json_file);
 
